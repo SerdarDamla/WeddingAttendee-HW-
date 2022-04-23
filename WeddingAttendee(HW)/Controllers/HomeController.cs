92e90187c8fc1553b2cf2e153dc9a9fddcc8800c
Serdar Damla <serdar.damla12@gmail.com>
2022-04-23 19:45:15 +0300

    Add project files.

---
+++ WeddingAttendee(HW)/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using introDotNetCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace introDotNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name1 = "Serdar";
+            ViewBag.Name2 = "Elif";
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(WeddingAttendee weddingAttendee)
+        {
+            ViewBag.Success=false;
+            if (ModelState.IsValid)
+            {
+                if (weddingAttendee.isComeWedding)
+                {
+                    ViewBag.Success = true;
+                }
+                
+                return View("ResultView");
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ WeddingAttendee(HW)/Models/WeddingAttendee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace introDotNetCore.Models
+{
+    public class WeddingAttendee
+    {
+        [Required(ErrorMessage ="Kullanıcı adı boş geçilemez")]
+        [MinLength(2,ErrorMessage ="En az iki karakter girilmelidir.")]
+        [Display(Name ="Kayıt Olacak Kişinin Adı-Soyadı")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="E-Posta boş geçilemez")]
+        [EmailAddress(ErrorMessage ="E-Posta tipi yanlış")]
+        [Display(Name = "Kayıt Olacak Kişinin E-Posta Adresi")]
+        public string Email { get; set; }
+
+        [Display(Name = "Düğüne katılacaksanız kaç kişi katılacaksınız?")]
+        public int HowManyPeople { get; set; }
+
+        [Required(ErrorMessage = "Gelip gelmeyeceğiniz alanı boş bırakmayınız.")]
+        [Display(Name = "Düğüne katılacak mısınız?")]
+        public bool isComeWedding { get; set; }
+    }
+}
